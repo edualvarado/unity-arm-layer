0a6d5b2798b8941d1ff81b52c05c3d963e7ab554
Eduardo Alvarado <33001422+edualvarado@users.noreply.github.com>
2021-01-18 17:12:55 +0100

    CHANGED: New models

---
+++ Assets/fixedPositionHips.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fixedPositionHips : MonoBehaviour
+{
+    [SerializeField] private Transform fixedHips;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //this.transform.position = fixedHips.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //this.transform.position = fixedHips.position;
+    }
+}
